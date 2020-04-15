79b10fb191ee6e0995ed32f364e03bf41df3c248
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-15 16:43:44 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  colors = ["red", "blue", "black", "purple"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  colors = ["red", "blue", "black", "purple"]
+  colors << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  colors = ["red", "blue", "black", "purple"]
+  colors.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  colors = ["red", "blue", "black", "arrays!"]
+  purple = colors.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  colors = ["wow", "blue", "black", "arrays!"]
+  wow = colors.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  colors = ["wow", "blue", "am", "arrays!"]
+  colors [2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  colors = ["wow", "blue", "am", "arrays!"]
+  colors.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  colors = ["wow", "blue", "am", "arrays!"]
+  colors.last
 end
