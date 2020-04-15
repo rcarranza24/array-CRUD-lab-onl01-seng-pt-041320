92b816c75dd2ae08e5c12a6eca3609b6f0e7f6e6
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-15 16:35:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   colors = ["red", "blue", "black", "purple"]
-  purple = colors.pop
+  arrays! = colors.pop
 end
 
 def remove_element_from_start_of_array(array)
