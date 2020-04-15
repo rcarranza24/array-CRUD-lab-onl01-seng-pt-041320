94b00e230067aa2d33ade58d90ec4957aa5380ac
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-15 16:30:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   colors = ["red", "blue", "black", "purple"]
-  colors << "gray"
+  colors << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
